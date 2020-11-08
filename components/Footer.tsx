d38830ae1135cd506de219d616a8b79932b37b81
Karthik RP <karthik.rp@hotmail.com>
2020-11-08 22:54:06 +0530

    Initial Commit

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/component/footer.module.scss';
+
+export default () => {
+    const year = (new Date()).getFullYear();
+    return (
+        <footer className={styles.footer}>
+            <h1>Dharani Foundation</h1>
+            <p> dharanifoundationbgm@gmail.com</p>
+            <p> 7760251535; 9845270380</p>
+            <p> No. 2, 4855, Sadashiv Nagar, Basavaraj Complex, APMC Main Road, Belagavi - 590001, Karnataka. </p>
+            <p>©{year} by Dharani Foundation.</p>
+        </footer>
+    )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import styles from '../styles/component/header.module.scss';
+import React, {useEffect, useState} from 'react';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
+export default () => {
+    const [navbarCollapsed, setNavBarCollapsed] = useState(false);
+    const collapseClass = navbarCollapsed ? 'collapse navbar-collapse' : null;
+
+    useEffect(() => {
+        setNavBarCollapsed(window.innerWidth < 500);
+    });
+
+    return (
+        <header className={styles.header}>
+            <Navbar bg="light" expand="lg" className="justify-content-between">
+                <Container className="my-4">
+                    <Navbar.Brand href="#home" className={styles.brand + ' brand'}>Dharani Foundation</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">About</Nav.Link>
+                            <Nav.Link href="#vision">Vision</Nav.Link>
+                            <Nav.Link href="#mission">Mission</Nav.Link>
+                            <Nav.Link href="#activities">Activities</Nav.Link>
+                            <Nav.Link href="#founders">Founders</Nav.Link>
+                            <Nav.Link href="#more">More</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
