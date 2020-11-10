35bbf0f3a702bacf33f1acadf1bdc1f5aee7513d
Karthik RP <karthik.rp@hotmail.com>
2020-11-10 18:41:39 +0530

    Addition of social links in footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import styles from '../styles/component/footer.module.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookF, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     const year = (new Date()).getFullYear();
@@ -8,7 +10,17 @@ const Footer = () => {
             <p> dharanifoundationbgm@gmail.com</p>
             <p> 7760251535; 9845270380</p>
             <p> No. 2, 4855, Sadashiv Nagar, Basavaraj Complex, APMC Main Road, Belagavi - 590001, Karnataka. </p>
-            <p>©{year} by Dharani Foundation.</p>
+
+            <div className="mt-2">
+                <a href="https://www.facebook.com/dharanifoundation" target="_blank">
+                    <FontAwesomeIcon icon={faFacebookF} className="mx-2"/>
+                </a>
+
+                <a href="https://www.instagram.com/dharanifoundation/" target="_blank">
+                    <FontAwesomeIcon icon={faInstagram} className="mx-2"/>
+                </a>
+            </div>
+            <p className="mt-2">©{year} by Dharani Foundation.</p>
         </footer>
     )
 }
